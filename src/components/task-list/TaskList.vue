e6b49a0c219c151452e96ae2f077407b51d741ba
Marcos Vinicius <marcosvcontato.mv@gmai.com>
2017-07-13 23:34:55 -0300

    Added new component of checkbox

--- src/components/task-list/TaskList.vue
+++ src/components/task-list/TaskList.vue
@@ -1,24 +1,29 @@
 <template>
     <ul class="todo-list">
         <li v-for="(task,index) in taskList" :key="index" class="todo">
-            <input class="toggle" :checked="task.completed" type="checkbox">
-            <label @click="completeTask(task)" :class="{'completed':task.completed}">{{task.title}}</label>
+            <checkbox :customModel="task.completed"></checkbox>
+            <label>{{task.title}}</label>
         </li>
     </ul>
 </template>
 
 <script>
+    import Checkbox from '../checkbox/Checkbox';
+    
     export default {
+        data() {
+            return {
+                checkModel: false
+            };
+        },
         props: {
             taskList: {
                 type: Array,
                 required: true
             }
         },
-        methods: {
-            completeTask(task) {
-                task.completed = !task.completed;
-            }
+        components: {
+            Checkbox
         }
     };
 </script>
@@ -37,25 +42,6 @@
                 padding-left: .5rem;
             }
             padding:.2rem;
-        }
-    }
-    
-    .toggle {
-        cursor: pointer;
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        -ms-progress-appearance: none;
-        outline: none;
-        transition: 1000ms;
-        &:checked::before {
-            content: url('../../assets/imgs/checked.svg');
-        }
-        &::before {
-            content: url('../../assets/imgs/no-checked.svg');
-        }
-        &:checked+label {
-            text-decoration: line-through;
-            font-style: italic;
         }
     }
 </style>
